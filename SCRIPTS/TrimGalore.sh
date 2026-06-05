9bb9d7bca37c38d880029f44cdffb649d21c735e
Rowena Couto <rowenacouto@Rowenas-MacBook-Pro.local>
2026-06-05 15:20:04 +0800

    TrimGalore.sh

--- SCRIPTS/TrimGalore.sh
+++ SCRIPTS/TrimGalore.sh
@@ -1,25 +1,24 @@
-#!/bin/bash
-
-FASTQ_DIR="/scratch/sbms006/rrubimsilvadocouto/rnaseq_trim/fastqs"
+FASTQ_DIR="/group/sbms006/rrubimsilvadocouto/MiSeq_Data/fastqs"
 OUT_DIR="/scratch/sbms006/rrubimsilvadocouto/rnaseq_trim/trimmed_all"
 
 mkdir -p "$OUT_DIR"
 
 for R1 in ${FASTQ_DIR}/*_R1_001.fastq.gz
 do
-    SAMPLE=$(basename "$R1" | sed 's/_R1_001.fastq.gz//')
-    R2=${FASTQ_DIR}/${SAMPLE}_R2_001.fastq.gz
+    SAMPLE=$(basename "$R1" _R1_001.fastq.gz)
+    R2="${FASTQ_DIR}/${SAMPLE}_R2_001.fastq.gz"
 
-    echo "======================================"
-    echo "Trimming sample: $SAMPLE"
-    echo "======================================"
+    if [ ! -f "$R2" ]; then
+        echo "Missing pair for $SAMPLE"
+        continue
+    fi
+
+    echo "Processing $SAMPLE"
 
     trim_galore \
         --paired \
         --fastqc \
         --output_dir "$OUT_DIR" \
         "$R1" "$R2"
-
 done
 
-echo "DONE: all samples trimmed"
